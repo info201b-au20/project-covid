61e2d1fef1db59eb63013ee4441c45b4a224bf36
Chandra <burnhc@uw.edu>
2020-11-11 18:59:53 -0800

    Updated table script

--- scripts/tables.r
+++ scripts/tables.r
@@ -6,7 +6,7 @@ race_data <- read.csv("./data/Race Data Entry - CRDT.csv")
 county_age_cases <-
   read_excel("./data/PUBLIC_CDC_EVENT_DATE_SARS.xlsx", sheet = 1)
 
-# Weekly new cases in each age group in WA
+# Weekly new cases by age group in WA
 weekly_county_age_cases <- county_age_cases %>%
   group_by(Date = WeekStartDate) %>%
   summarize(`Age 0-19` = sum(`Age 0-19`),
@@ -15,8 +15,8 @@ weekly_county_age_cases <- county_age_cases %>%
             `Age 60-79` = sum(`Age 60-79`),
             `Age 80+` = sum(`Age 80+`))
 
-# Cumulative (total) cases of COVID for racial groups in WA
-wa_race_data <- race_data %>%
+# Cumulative (total) cases of COVID by racial group in WA
+cumulative_cases_by_race <- race_data %>%
   filter(State == "WA") %>%
   arrange(Date) %>%
   summarize(Date = as.Date(as.character(Date), format = "%Y%m%d"),
@@ -28,8 +28,42 @@ wa_race_data <- race_data %>%
             "Native Hawaiian/Pacific Islander" = Cases_NHPI,
             Multiracial = Cases_Multiracial,
             Other = Cases_Other,
-            Unknown = Cases_Unknown)
+            Unknown = Cases_Unknown) %>%
+  # Excluded first 9 rows due to irregularity
+  slice(- (1:7))
+
+# Rate of increase of new COVID cases by racial group in WA (percent)
+rate_cases_by_race <- race_data %>%
+  filter(State == "WA") %>%
+  arrange(Date) %>%
+  summarize(Date = as.Date(as.character(Date), format = "%Y%m%d"),
+            White = ((as.numeric(Cases_White) - as.numeric(
+              lag(Cases_White))) / as.numeric(lag(Cases_White))) * 100,
+            Black = ((as.numeric(Cases_Black) - as.numeric(
+              lag(Cases_Black))) / as.numeric(lag(Cases_White))) * 100,
+            "Hispanic/Latino" = ((as.numeric(Cases_LatinX) - as.numeric(
+              lag(Cases_LatinX))) / as.numeric(lag(Cases_LatinX))) * 100,
+            Asian = ((as.numeric(Cases_Asian) - as.numeric(
+              lag(Cases_Asian))) / as.numeric(lag(Cases_Asian))) * 100,
+            "American Indian/Alaska Native" = ((
+              as.numeric(Cases_AIAN) - as.numeric(
+                lag(Cases_AIAN))) / as.numeric(lag(Cases_AIAN))) * 100,
+            "Native Hawaiian/Pacific Islander" = ((
+              as.numeric(Cases_NHPI) - as.numeric(
+                lag(Cases_NHPI))) / as.numeric(lag(Cases_NHPI))) * 100,
+            Multiracial = ((
+              as.numeric(Cases_Multiracial) - as.numeric(
+                lag(Cases_Multiracial))) / as.numeric(
+                  lag(Cases_Multiracial))) * 100,
+            Other = ((as.numeric(Cases_Other) - as.numeric(
+              lag(Cases_Other))) / as.numeric(lag(Cases_Other))) * 100,
+            Unknown = ((as.numeric(Cases_Unknown) - as.numeric(
+              lag(Cases_Unknown))) / as.numeric(lag(Cases_Unknown))) * 100) %>%
+  mutate_if(is.numeric, round, digits = 1) %>%
+  # Excluded first 9 rows due to irregularity
+  slice(- (1:9))
 
 # Displaying the tables
 kable(weekly_county_age_cases)
-kable(WA_race_data)
+kable(cumulative_cases_by_race)
+kable(rate_cases_by_race)
