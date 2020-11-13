423d9893f896a417dac5628f72812f346152fb64
Minjie Kim <minjkim2@uw.edu>
2020-11-13 14:21:41 -0800

    Add Chart Description

--- scripts/ViolinPlot.R
+++ scripts/ViolinPlot.R
@@ -1,6 +1,12 @@
 library("ggplot2")
 source("tables.r")
 
+#The purpose of including this chart is to see how different age groups are
+#affected by Covid-19, and to see which groups contribute most the number of
+#cases present. In this plot we are able to that the most mean number of weekly
+#cases is from the age 20-39 group, this group is also has the max number of 
+#cases in a pparticular week during the time of data collection
+
 # get average cases per week for each age group
 avg_weekly_cases_age <- weekly_cases_by_age %>%
   select(`Age 0-19`,
@@ -14,7 +20,7 @@ avg_weekly_cases_age <- weekly_cases_by_age %>%
 
 plot <- ggplot(data = avg_weekly_cases_age, aes(x = age_group, y = weekly_cases)) + 
   geom_violin() +
-  labs(title = "Distribution of Weekly CoVid Cases for each Age Group",
+  labs(title = "Distribution of Weekly Covid Cases for each Age Group",
        x = "Age Group", 
        y = "Weekly Cases")
 
