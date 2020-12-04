94f12646b0fb837f19a1780438bcaf50141f6cf9
unknown <ley5@uw.edu>
2020-12-04 22:30:23 +0800

    add Grant's page, need to intergrate to the main app

---
+++ scripts/grant_page.R
@@ -0,0 +1,91 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(dplyr)
+library(shiny)
+library(ggplot2)
+library(plotly)
+
+
+data <- read.csv("../data/Race Data Entry - CRDT.csv")
+
+build_smooth_num_death <- function(data, state_name){
+    race_data <- data %>% 
+         filter(State == state_name)
+    race_data$Date <- as.Date(paste(substr(race_data$Date, 1, 4), substr(race_data$Date, 5, 6), "01", sep = "-"))
+
+    smooth_plot_chart <- ggplot(race_data) +
+        geom_smooth(aes(x = Date, y = Deaths_Total))+
+        scale_x_date(date_breaks = "1 month")+
+        labs(
+            x='Time',
+            y='Total Death',
+            title = "Total Death OverTime"
+        )
+    
+}
+
+build_smooth_num_death(data, "CO")
+
+
+###UI#####
+
+stateNames <- data %>% 
+    select(State) %>% 
+    distinct(State)
+    
+selectState <- selectInput(
+    "state_name",
+    label = "Select A State",
+    choices = stateNames
+)
+
+###
+
+
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Total Number of Death Over Time for State"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectState
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotlyOutput('plot')
+           
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+
+    output$plot <- renderPlotly({
+        plot <- build_smooth_num_death(data, input$state_name)
+        ggplotly(plot)
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
+
+
