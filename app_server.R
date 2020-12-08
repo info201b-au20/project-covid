944f86efffc78ee3c550e83f996f07a9793dfb43
Jacquie Si <jacquiesi@Jacquies-MacBook-Pro.local>
2020-12-08 08:09:52 +0800

    add jackie si

--- app_server.R
+++ app_server.R
@@ -2,25 +2,30 @@ library(shiny)
 library(tidyverse)
 library(plotly)
 
-black_data <- read.csv("https://github.com/info201b-au20/project-covid/blob/gh-pages/data/Race%20Data%20Entry%20-%20CRDT.csv")
-unique(black_data$State)
+race_data <- read.csv("../data/Race Data Entry - CRDT.csv")
+unique(race_data$State)
 
+## For the second page by Jackie Si.
+# Filtering out useful data for the second page based on selected state.
+make_plot <- function(data, color_input, state_selected){
+      black_data <- race_data %>%
+        filter(State == state_selected)
+ # Make plot using ggplot
+      plot_covid <- ggplot(data = black_data) +
+        geom_smooth(
+        mapping = aes_string(x = black_data$Date, y = black_data$Cases_Black),
+        color = color_input
+      ) +
+      ggtitle("Compare the number of cases of African Americans in WA/CA from April to October 2020") +
+      labs(x = "Date(month)", y = "Cases")
+}
+
+# Server
 server <- function(input, output) {
-  output$startdate <- renderText({
-    as.character(input$daterange[1])
-  })
-  output$enddate <- renderText({
-    as.character(input$daterange[2])
-  })
-  output$range <- renderText({
-    paste("Selected date range is", input$daterange[1], "to", input$daterange[2])
-  })
+  black_data$Date <- as.Date(paste(substr(black_data$Date, 1, 4), substr(black_data$Date, 5, 6), "01", sep = "-"))
+
   output$plot <- renderPlotly({
-    p <- black_data %>%
-      filter(State %in% input$State) %>%
-      plot_ly(x = ~Date, y = ~Cases_Black, color = ~State)
-    p
-  }) 
-   %>%
-     layout(title = "Compare the number of cases of African Americans in WA/NY/CA")
+    plot_covid <- make_plot(black_data, input$color_input, input$Statename)
+    ggplotly(plot_covid)
+  })
 }
