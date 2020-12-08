aa20a2d601d305c7d0ef4f0f1fe9683c5238ee8f
unknown <ley5@uw.edu>
2020-12-08 08:43:16 +0800

    Add server for first page

--- app_server.R
+++ app_server.R
@@ -21,9 +21,34 @@ make_plot <- function(data, color_input, state_selected){
       labs(x = "Date(month)", y = "Cases")
 }
 
+###############################################
+## For the first page by GRANT 
+build_smooth_num_death <- function(data, state_name, color_selected){
+  race_data <- data %>% 
+    filter(State == state_name)
+  race_data$Date <- as.Date(paste(substr(race_data$Date, 1, 4), substr(race_data$Date, 5, 6), "01", sep = "-"))
+  
+  smooth_plot_chart <- ggplot(race_data) +
+    geom_smooth(aes(x = Date, y = Deaths_Total), color = color_selected, na.rm = TRUE)+
+    scale_x_date(date_breaks = "1 month")+
+    labs(
+      x='Time',
+      y='Total Death',
+      title = "Total Death OverTime"
+    )
+}
+
+
+##############################################
 # Server
 server <- function(input, output) {
+  #####First Page #######
+  output$plot <- renderPlotly({
+    plot <- build_smooth_num_death(race_data, input$state_name, input$color_selected)
+    ggplotly(plot)
+  })
   
+  #####Second Page######
   output$plot <- renderPlotly({
     plot_covid <- make_plot(black_data, input$color_input, input$Statename)
     ggplotly(plot_covid)
